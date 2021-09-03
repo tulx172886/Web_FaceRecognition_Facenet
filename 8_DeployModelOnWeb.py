7c21e817d82b4f97580d324137e23a02f467dfc8
letu0712 <letu071299@gmail.com>
2021-09-03 10:12:53 +0100

    update 3/9

--- 8_DeployModelOnWeb.py
+++ 8_DeployModelOnWeb.py
@@ -6,6 +6,9 @@ from keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 import pickle
 import os
+import tensorflow as tf
+
+print(tf.config.list_physical_devices('GPU'))
 
 face_detector = mtcnn.MTCNN()
 model_embedding = load_model("facenet_keras.h5")
@@ -42,49 +45,63 @@ def index():
         img_rgb = cv2.cvtColor(img_org, cv2.COLOR_BGR2RGB)
         pixels = np.asarray(img_rgb)
         results = face_detector.detect_faces(img_rgb)
+        print(results)
+        print("Số khuôn mặt phát hiện trong ảnh: ", len(results))
         if results:
-            x1, y1, width, height = results[0]['box']
-            x1, y1 = abs(x1), abs(y1)
-            x2, y2 = x1 + width, y1 + height
-            face = pixels[y1:y2, x1:x2]
-            face = cv2.resize(face, (160,160))
-            face_pixels = face.astype('float32')
+            # colors = [(0,255,0), (0,0,255),(255,0,0),(255,255,0),(0,255,255)]
+            colors = np.random.randint(0,255,(len(results), 3))
+            print(colors)
+            for i in range(len(results)):
+                print(i)
+                if results[i]['confidence'] > 0.95:
+                    x1, y1, width, height = results[i]['box']
+                    x1, y1 = abs(x1), abs(y1)
+                    x2, y2 = x1 + width, y1 + height
+                    face = pixels[y1:y2, x1:x2]
+                    face = cv2.resize(face, (160,160))
+                    face_pixels = face.astype('float32')
 
-            #Chuẩn hóa các pixels
-            mean , std = face_pixels.mean(), face_pixels.std()
-            face_pixels = (face_pixels - mean) / std
-            samples = np.expand_dims(face_pixels, axis=0)
-            samples = model_embedding.predict(samples)       #face embedding
+                    #Chuẩn hóa các pixels
+                    mean , std = face_pixels.mean(), face_pixels.std()
+                    face_pixels = (face_pixels - mean) / std
+                    samples = np.expand_dims(face_pixels, axis=0)
+                    samples = model_embedding.predict(samples)       #face embedding
 
-            #Predict
-            yhat_class = model.predict(samples)             # Predict label
-            yhat_prob = model.predict_proba(samples)         #Xác suất
+                    #Predict
+                    yhat_class = model.predict(samples)             # Predict label
+                    yhat_prob = model.predict_proba(samples)         #Xác suất
 
-            #Chuyển đổi thành dữ liệu để hiển thị
-            class_index = yhat_class[0]
-            class_probability = yhat_prob[0, class_index] * 100
-            predict_name = out_encoder.inverse_transform(yhat_class)
+                    #Chuyển đổi thành dữ liệu để hiển thị
+                    class_index = yhat_class[0]
+                    print("Class index: ",class_index)
+                    class_probability = yhat_prob[0, class_index] * 100
+                    predict_name = out_encoder.inverse_transform(yhat_class)
 
-            print("Predict: %s" %(predict_name[0]))
-            print("Probability: %.3f" %(class_probability))
+                    print("Predict: %s" %(predict_name[0]))
+                    print("Probability: %.3f" %(class_probability))
 
-            cv2.rectangle(img_org, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            if class_probability > 95:
-                cv2.putText(img_org, predict_name[0] + " " + str(round(class_probability, 2)), (x1, y1),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.imwrite(image_path, img_org)
-                return render_template("index.html", imgname=image.filename,
-                                                    facename=predict_name[0],
-                                                    probability=class_probability,
-                                                    success=True)
-            else:
-                cv2.putText(img_org, "Unknown", (x1, y1),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.imwrite(image_path, img_org)
-                return render_template("index.html", imgname=image.filename,
-                                                    facename="Unknown",
-                                                    probability="",
-                                                    success=True)
+                    color = (int(colors[class_index][0]), int(colors[class_index][1]), int(colors[class_index][2]))
+                    print(color)
+                    cv2.rectangle(img_org, (x1, y1), (x2, y2), color, 2)
+                    if class_probability > 95:
+                        cv2.putText(img_org, predict_name[0] + " " + str(round(class_probability, 2)), (x1, y1),
+                                    cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+                    else:
+                        cv2.putText(img_org, "Unknown", (x1, y1),
+                                    cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+            cv2.imwrite(image_path, img_org)
+            return render_template("index.html", imgname=image.filename,
+                                                # facename=predict_name[0],
+                                                # probability=class_probability,
+                                                success=True)
+                    # else:
+                    #     cv2.putText(img_org, "Unknown", (x1, y1),
+                    #                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    #     cv2.imwrite(image_path, img_org)
+                    #     return render_template("index.html", imgname=image.filename,
+                    #                                         facename="Unknown",
+                    #                                         probability="",
+                    #                                         success=True)
 
 
         else:
